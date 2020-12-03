08afb5cb196f95ff7c99b5e7c31d6ba19d536d96
Thomas Whiteway <thomas.whiteway@gmail.com>
2020-12-03 21:15:46 +0000

    Day 3 part 2

--- day3/app/Main.hs
+++ day3/app/Main.hs
@@ -4,8 +4,19 @@ istree :: Char -> Bool
 istree '#' = True
 istree _ = False
 
+gradients :: [(Int, Int)]
+gradients = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)]
+
 solve :: [String] -> Int
-solve slope = length $ filter istree [Main.lookup slope (3 * y)  y | y <- [0..length slope-1]]
+solve slope = product $ map (numTrees slope) gradients
+
+numTrees :: [String] -> (Int, Int) -> Int
+numTrees slope (dx, dy) = length $ filter istree $ map (uncurry $ Main.lookup slope) $ takeWhile (\(_, y) -> y < length slope) [(dx * i, dy * i) | i <- [0..]]
 
 lookup :: [String] -> Int -> Int -> Char
 lookup slope x y = row !! (x `mod` length row)
