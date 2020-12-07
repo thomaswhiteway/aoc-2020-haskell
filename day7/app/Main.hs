4cf920f7bb631d20e6fa004e536eb6fc3d51a923
Thomas Whiteway <thomas.whiteway@gmail.com>
2020-12-07 19:42:23 +0000

    Day 7 Part 2

--- day7/app/Main.hs
+++ day7/app/Main.hs
@@ -2,6 +2,10 @@ module Main where
 
 import Text.Parsec (parse, eof, endBy, newline, many1, digit, sepBy, char, lower, string, optional, (<|>))
 import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Control.Monad (liftM2)
+import Data.Maybe (fromJust)
+import Data.Either (partitionEithers)
 
 type Bag = String;
 
@@ -39,6 +43,28 @@ canContain b rs = Set.unions (Set.fromList containers:[canContain c rs | c <- co
     where
         containers = map fst $ filter (contains b) rs
 
+foldrMaybes :: (a -> b -> b) -> b -> [Maybe a] -> Maybe b
+foldrMaybes f init = foldr (liftM2 f) (Just init)
+
+numContains :: Bag -> [Rule] -> Int
+numContains b rs = fromJust $ Map.lookup b $ allContains rs
+
+allContains :: [Rule] -> Map.Map Bag Int
+allContains rs = allContains' rs Map.empty
+
+allContains' :: [Rule] -> Map.Map Bag Int -> Map.Map Bag Int
+allContains' [] m = m
+allContains' rs' m = allContains' remaining (m `Map.union` Map.fromList new)
+    where
+        (new, remaining) = partitionEithers [ case numContains' cs m of 
+            Nothing -> Right (c, cs)
+            Just n -> Left (c, n) | (c, cs) <- rs']
+
+numContains' :: [(Int, Bag)] -> Map.Map Bag Int -> Maybe Int           
+numContains' contents counts = foldrMaybes (+) 0
+    [fmap (\contained -> required * (contained + 1)) $ Map.lookup bag counts 
+        | (required, bag) <- contents]
+
 main :: IO ()
-main = interact (show . Set.size . canContain "shiny gold" . parseRules)
+main = interact (show . numContains "shiny gold" . parseRules)
 
