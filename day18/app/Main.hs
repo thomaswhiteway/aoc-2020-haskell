eef6277e2d891cb56bb927f5fc366b5a21ba9a2e
Thomas Whiteway <thomas.whiteway@gmail.com>
2020-12-18 15:01:41 +0000

    Day 18 Part 2

--- day18/app/Main.hs
+++ day18/app/Main.hs
@@ -1,45 +1,20 @@
 module Main where
 
-import Text.Parsec (string, (<|>), many1, digit, try, newline, endBy, choice, parse, eof, ParsecT, notFollowedBy)
 import Data.Functor.Identity (Identity)
 import Data.Char (isSpace, isDigit)
+import Data.Bifunctor (first)
+import Data.Maybe (mapMaybe, fromJust)
 
-data Operator = Add | Mul deriving (Show)
-data Expression = Constant Int | Calculation Expression Operator Expression deriving (Show)
+data Op = Add | Mul deriving (Show, Eq)
+data Expression = Constant Int | Calculation Expression Op Expression deriving (Show)
 
-apply :: Operator -> Int -> Int -> Int
+data Token = LeftBracket | RightBracket | TokAdd | TokMul  | Num Int deriving (Eq, Show)
+
+data Tree v a = Node v [Tree v a] | Leaf a deriving (Eq, Show)
+
+apply :: Op -> Int -> Int -> Int
 apply Add = (+)
 apply Mul = (*)
-
-operator :: ParsecT String u Identity Operator
-operator = do { string "+"; return Add } <|> do { string "*"; return Mul } 
-
-number :: ParsecT String u Identity Int
-number = read <$> many1 digit
-
-parse3 :: Monad m => (t1 -> t2 -> t3 -> b) -> m t1 -> m t2 -> m t3 -> m b
-parse3 f a b c = do
-    a' <- a
-    b' <- b
-    f a' b' <$> c
-
-constant :: ParsecT String u Identity Expression
-constant = Constant <$> number
-
-leftCalculation :: ParsecT String u Identity Expression
-leftCalculation = parse3 Calculation constant operator expression
-
-rightCalculation :: ParsecT String u Identity Expression
-rightCalculation = parse3 Calculation bracketedExpression operator expression
-
-bracketedExpression :: ParsecT String u Identity Expression
-bracketedExpression = string "(" *> expression <* string ")"
-
-expression :: ParsecT String u Identity Expression
-expression = choice [ try (constant <* notFollowedBy operator)
-                    , try leftCalculation
-                    , try rightCalculation
-                    , try bracketedExpression ]
 
 resolve :: Expression -> Int
 resolve (Constant x) = x
@@ -50,24 +25,71 @@ resolve (Calculation x op y) = resolve x `op'` resolve y
 tokenize [] = []
 tokenize (c:cs) 
     | c == ' ' = tokenize cs
-    | c == '(' = ")":tokenize cs
-    | c == ')' = "(":tokenize cs
-    | num /= "" =  num:tokenize rest
-    | otherwise = [c]:tokenize cs
+    | c == '(' = LeftBracket:tokenize cs
+    | c == ')' = RightBracket:tokenize cs
+    | c == '+' = TokAdd:tokenize cs
+    | c == '*' = TokMul:tokenize cs
+    | num /= "" =  Num (read num):tokenize rest
     where 
         (num, rest) = span isDigit (c:cs)
 
-
-preprocess = concat . reverse . tokenize 
-
-parseExpression :: String -> Expression
-parseExpression text = case parse (expression <* eof) "" text' of
-    Left e -> error $ "Failed to parse expression: " ++ show e
-    Right es -> es
+parseBracket :: [Token] -> ([Tree (Maybe Op) Token], [Token])
+parseBracket [] = ([], [])
+parseBracket (LeftBracket:toks) = (Node Nothing nested : rest', toks')
     where
-        text' = preprocess text
+        (nested, rest) = parseBracket toks
+        (rest', toks') = parseBracket rest
+parseBracket (RightBracket:toks) = ([], toks)
+parseBracket (other:toks) = (Leaf other : rest, toks')
+    where
+        (rest, toks') = parseBracket toks
+
+
+parseBrackets :: [Token] -> Tree (Maybe Op) Token
+parseBrackets = Node Nothing . fst . parseBracket 
+
+
+recurse :: ([Tree (Maybe a) l] -> Tree (Maybe a) l) -> Tree (Maybe a) l -> Tree (Maybe a) l
+recurse _ (Leaf t) = Leaf t
+recurse f (Node (Just x) children) = Node (Just x) $ map (recurse f) children
+recurse f (Node Nothing children) = f $ map (recurse f) children
+
+splitOn :: Eq a => a -> [a] -> Maybe ([a], [a])
+splitOn val [] = Nothing
+splitOn val (x:xs) = if x == val 
+                   then Just ([], xs) 
+                   else first (x:) <$> splitOn val xs 
+
+parseOp :: Token -> Op -> Tree (Maybe Op) Token -> Tree (Maybe Op) Token
+parseOp token op = recurse parseOp'
+    where 
+        parseOp' trees = case splitOn (Leaf token) trees of
+            Just (before, after) -> Node (Just op) [ parseOp' before, parseOp' after ]
+            Nothing -> Node Nothing trees
+
+parseAddition :: Tree (Maybe Op) Token -> Tree (Maybe Op) Token
+parseAddition = parseOp TokAdd Add
+
+parseMultiplication :: Tree (Maybe Op) Token -> Tree (Maybe Op) Token
+parseMultiplication = parseOp TokMul Mul
+
+normTree :: Tree (Maybe a) b -> Maybe (Tree (Maybe a) b)
+normTree (Leaf l) = Just $ Leaf l
+normTree (Node (Just op) children) = Just $ Node (Just op) $ mapMaybe normTree children
+normTree (Node Nothing children) = case mapMaybe normTree children of
+    [] -> Nothing
+    [t] -> Just t
+    ts -> Just $ Node Nothing ts
+
+compile :: Tree (Maybe Op) Token -> Expression
+compile (Leaf (Num num)) = Constant num
+compile (Node (Just op) [x, y]) = Calculation (compile x) op (compile y)
+
+parseExpression :: [Token] -> Expression
+parseExpression = compile . fromJust . normTree . parseAddition . parseMultiplication . parseBrackets
 
 main :: IO ()
 main = do
     ls <- fmap lines getContents
-    print $ sum $ map (resolve . parseExpression) ls
+    mapM_ (print . resolve . parseExpression . tokenize) ls
+    print $ sum $ map (resolve . parseExpression. tokenize) ls
