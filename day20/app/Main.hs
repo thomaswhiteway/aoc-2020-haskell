7b32e67190e0ee4ab1440863066aa1a790074541
Thomas Whiteway <thomas.whiteway@gmail.com>
2020-12-20 15:00:21 +0000

    Day 20 Part 2

--- day20/app/Main.hs
+++ day20/app/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 
 import Text.Parsec (parse, digit, string, newline, char, many1, char, (<|>), endBy, ParsecT, eof)
-import Data.List ( find )
+import Data.List ( find, delete, intercalate )
 import Data.Maybe ( fromJust, fromMaybe )
 import Data.Bits ( Bits((.|.), (.&.)), shift, testBit)
 import Data.IntMap.Lazy (IntMap, (!))
 import qualified Data.IntMap.Lazy as IntMap
-
+import Data.Map.Lazy (Map, (!?))
+import qualified Data.Map.Lazy as Map
+import Debug.Trace (trace)
 type Signature = (Int, Int, Int, Int)
 type TileId = Int
 
@@ -21,10 +23,10 @@ tile = do
 
 tiles = tile `endBy` newline
 
-parseTiles :: String -> [(TileId, [String])]
+parseTiles :: String -> IntMap [String]
 parseTiles text = case parse (tiles <* eof) "" text of 
     Left e -> error $ "Failed to parse tiles: " ++ show e
-    Right ts -> ts
+    Right ts -> IntMap.fromList ts
 
 fromBinary :: [Int] -> Int
 fromBinary [] = 0
@@ -36,36 +38,23 @@ toBinary x = [ if testBit x i then 1 else 0 | i <- reverse [0..9]]
 sig :: String -> Int
 sig line = fromBinary [if c == '#' then 1 else 0 | c <- line]
 
-getSides :: [String] -> Signature
-getSides tile = (sig $ head tile,
+getSignature :: [String] -> Signature
+getSignature tile = (sig $ head tile,
                  sig $ map last tile,
                  sig $ last tile,
                  sig $ map head tile)
 
-flip :: Int -> Int
-flip = fromBinary . reverse . toBinary
+north :: Signature -> Int
+north (n, _, _, _) = n
 
-flipVertical :: Signature -> Signature
-flipVertical (n, e, s, w) = (s, Main.flip e, n, Main.flip w)
+east :: Signature -> Int
+east (_, e, _, _) = e
 
-flipHorizontal :: Signature -> Signature
-flipHorizontal (n, e, s, w) = (Main.flip n, w, Main.flip s, e)
+south :: Signature -> Int
+south (_, _, s, _) = s
 
-flips :: Signature -> [Signature]
-flips x = [x, flipHorizontal x, flipVertical x, flipHorizontal $ flipVertical x]
-
-rotate :: Signature -> Signature
-rotate (n, e, s, w) = (w, n, e, s)
-
-rotateN :: Int -> Signature -> Signature
-rotateN 0 x = x
-rotateN n x = rotateN (n-1) $ rotate x
-
-rotations :: Signature -> [Signature]
-rotations x = [ rotateN i x | i <- [0..3] ]
-
-getPermutations :: Signature -> [Signature]
-getPermutations sig = [ p | r <- rotations sig, p <- flips r ]
+west :: Signature -> Int
+west (_, _, _, w) = w
 
 sqrt :: Int -> Int
 sqrt x = fromJust $ find (\y -> y*y == x) [1..]
@@ -73,28 +62,126 @@ sqrt x = fromJust $ find (\y -> y*y == x) [1..]
 insert :: Eq a => a -> [a] -> [a]
 insert x xs = if x `elem` xs then xs else x:xs
 
-buildSigMap :: IntMap [Signature] -> IntMap [TileId]
-buildSigMap sigs = foldr (\(tileid, sig) m -> IntMap.alter (Just . insert tileid . fromMaybe []) sig m) IntMap.empty
+buildedgeToTileIds :: IntMap [Signature] -> IntMap [TileId]
+buildedgeToTileIds sigs = foldr (\(tileid, sig) m -> IntMap.alter (Just . insert tileid . fromMaybe []) sig m) IntMap.empty
     [ (tileid, d) | (tileid, sigs') <- IntMap.toList sigs, (n, e, s, w) <- sigs', d <- [n, e, s, w] ]
 
 isCorner :: IntMap [TileId] -> TileId -> Signature -> Bool
-isCorner sigMap tid (n, e, s, w) = (isEdge tid n && isEdge tid e) || (isEdge tid e && isEdge tid s) || (isEdge tid s && isEdge tid w) || (isEdge tid w && isEdge tid n)
+isCorner edgeToTileIds tid (n, e, s, w) = any (\(d1, d2) -> isEdge' d1 && isEdge' d2) [(n, e), (e, s), (s, w), (w, n)]
     where
-        isEdge :: TileId -> Int -> Bool
-        isEdge tid d = length (sigMap ! d) == 1
+        isEdge' = isEdge edgeToTileIds tid
+
+isEdge :: IntMap [TileId] -> TileId -> Int -> Bool
+isEdge edgeToTileIds tid d = length (edgeToTileIds ! d) == 1
 
 findCorners :: IntMap [Signature] -> IntMap [TileId] -> [TileId]
-findCorners tiles sigMap = [ tileid | (tileid, sigs) <- IntMap.toList tiles
-                                    , all (isCorner sigMap tileid) sigs ]
+findCorners tiles edgeToTileIds = [ tileid | (tileid, sigs) <- IntMap.toList tiles
+                                           , all (isCorner edgeToTileIds tileid) sigs ]
+
+indent :: Int -> String -> String
+indent n text =  replicate (n * 2) ' ' ++ text
+
+placeTiles :: (Int, Int) -> IntMap [Signature] -> IntMap [TileId] -> [Map (Int, Int) (TileId, Signature)]
+placeTiles (width, height) tileIdToSigs edgeToTileIds = placeTiles' Map.empty (IntMap.keys tileIdToSigs) [(x, y) | y <- [0..width-1], x <- [0..height-1]]
+    where
+
+        placeTiles' :: Map (Int, Int) (TileId, Signature) -> [TileId] -> [(Int, Int)] -> [Map (Int, Int) (TileId, Signature)]
+        placeTiles' current []      []         = [current]
+        placeTiles' current tileids (pos@(x, y):poss) = [ sol | (tileid, sig) <- possibleTiles current tileids pos 
+                                                              , sol <- placeTiles' (placeTile tileid sig pos current) (delete tileid tileids) poss ]
+
+        placeTile :: TileId -> Signature -> (Int, Int) -> Map (Int, Int) (TileId, Signature) -> Map (Int, Int) (TileId, Signature) 
+        placeTile tileid sig pos m =  Map.insert pos (tileid, sig) m
+            
+        possibleTiles :: Map (Int, Int) (TileId, Signature) -> [TileId] -> (Int, Int) -> [(TileId, Signature)]                                                       
+        possibleTiles current tileids pos = [ (tileid, sig) | tileid <- tileids
+                                                            , sig <- tileIdToSigs ! tileid 
+                                                            , fits current pos tileid sig ]
+
+        fits current pos tileid sig = all (\f -> f current pos tileid sig) [fitsN, fitsE, fitsS, fitsW]
+        fitsN current  (x, y) tileid (n, _, _, _)
+            | y == 0          = isEdge edgeToTileIds tileid n
+            | otherwise       = maybe True (\(_, sig) -> n == south sig) (current !? (x, y-1))
+        fitsE current (x, y) tileid (_, e, _, _) 
+            | x == (width-1)  = isEdge edgeToTileIds tileid e
+            | otherwise       = maybe True (\(_, sig) -> e == west  sig) (current !? (x+1, y))
+        fitsS current  (x, y) tileid (_, _, s, _)
+            | y == (height-1) = isEdge edgeToTileIds tileid s
+            | otherwise       = maybe True (\(_, sig) -> s == north sig) (current !? (x, y+1))
+        fitsW current (x, y) tileid (_, _, _, w) 
+            | x == 0          = isEdge edgeToTileIds tileid w
+            | otherwise       = maybe True (\(_, sig) -> w == east  sig) (current !? (x-1, y))            
+
+rotate []           = []
+rotate [line]       = map (:[]) $ reverse line
+rotate (line:lines) = zipWith (:) (reverse line) (rotate lines) 
+
+rotateN 0 square = square
+rotateN n square = rotateN (n-1) $ rotate square
+
+rotations square = [ rotateN n square | n <- [0..3] ]
+
+flipHorizontal = map reverse
+flipVertical = reverse
+
+flips square = [ square, flipHorizontal square, flipVertical square, flipHorizontal $ flipVertical square ]
+
+getPermutations :: [String] -> [[String]]
+getPermutations square = [ sq | r <- rotations square, sq <- flips r ]
+
+findPermutation :: [String] -> Signature -> [String]
+findPermutation square sig = fromJust $ find (\sq -> getSignature sq == sig) $ getPermutations square
+
+flatten :: [[[String]]] -> [String]
+flatten = concatMap (foldr (zipWith (++)) (replicate 10 [])) 
+
+renderMap :: (Int, Int) -> IntMap [String] -> Map (Int, Int) (TileId, Signature) -> [String]
+renderMap (width, height) tiles positions = flatten [[ trimCrusts $ findTile (x, y) | x <- [0..width-1]] | y <- [0..height-1]]
+    where
+        findTile :: (Int, Int) -> [String]
+        findTile pos = findPermutation (tiles ! tileid) sig
+            where
+                Just (tileid, sig) = positions !? pos
+        trimCrusts :: [String] -> [String]
+        trimCrusts = map (tail . init) . tail . init
+
+seaMonster :: [String]
+seaMonster = lines "                  # \n#    ##    ##    ###\n #  #  #  #  #  #   "
+
+countPattern matcher [] = 0
+countPattern matcher text@(_:rest) = countRowMatches matcher text + countPattern matcher rest
+
+countRowMatches matcher text 
+    | length text < length matcher = 0
+    | length (head text) < length (head matcher) = 0
+    | otherwise  = (if isMatch matcher text then 1 else 0) + countRowMatches matcher (map tail text)
+
+isMatch matcher text = and $ zipWith (\m l -> and $ zipWith ($) m l) matcher text
+
+buildChecker text = [[if c == ' ' then const True else (==c) | c <- line] | line <- text]
+
+countSeaMonsters :: [String] -> Int
+countSeaMonsters = countPattern (buildChecker seaMonster)
+
+count f = length . filter f
 
 main :: IO ()
 main = do
-    ts <- fmap parseTiles getContents 
-    print $ length ts
-    let sideLength = Main.sqrt $ length ts
-    print sideLength
-    let ts' = IntMap.fromList [ (index, getPermutations $ getSides tile) | (index, tile) <- ts ]
-    let sigMap = buildSigMap ts'
-    let corners = findCorners ts' sigMap
+    tiles <- fmap parseTiles getContents 
+    print $ length tiles
+    let side = Main.sqrt $ length tiles
+    print side
+    let tileIdToSigs = IntMap.map (map getSignature . getPermutations) tiles
+    let edgeToTileIds = buildedgeToTileIds tileIdToSigs
+    let corners = findCorners tileIdToSigs edgeToTileIds
     putStrLn $ "Have " ++ show (length corners) ++ " corners"
     print $ product corners
+    case placeTiles (side, side) tileIdToSigs edgeToTileIds of
+        [] -> putStrLn "No Solution"
+        (m:_) -> do
+            let display = renderMap (side, side) tiles m
+            putStrLn $ unlines display
+            let seaMonsters = maximum $ map countSeaMonsters $ getPermutations display
+            putStrLn $ "Found " ++ show seaMonsters ++ " sea monsters\n"
+            let roughness = count (=='#') (unlines display) - seaMonsters * count (=='#') (unlines seaMonster)
+            putStrLn $ "Roughness " ++ show roughness
