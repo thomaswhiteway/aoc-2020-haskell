7a17ea882e907cb06c3d8a07d08fd1c37d98e06e
Thomas Whiteway <thomas.whiteway@gmail.com>
2020-12-23 22:38:25 +0000

    Day 23 Remove duplication

--- day23/app/Main.hs
+++ day23/app/Main.hs
@@ -2,15 +2,13 @@ module Main where
 
 import Data.Array ( Array, (!))
 import Data.Array.IO (IOUArray)
-import Data.Array.MArray (MArray , readArray, writeArray, freeze)
+import Data.Array.MArray (MArray (getBounds) , readArray, writeArray, freeze)
 import Data.Char (digitToInt)
 import Data.List (sort, intercalate)
 import qualified Data.Array as Array
 import qualified Data.Array.MArray as MArray
 import System.Environment (getArgs)
-import Util (nTimes)
 import Data.Foldable (toList)
-import Data.Time.Clock (getCurrentTime)
 
 findNext :: Int -> Int -> [Int] -> Int
 findNext m 0 xs = findNext m m xs
@@ -30,9 +28,10 @@ follow n buffer x = do
     rest <- follow (n-1) buffer x'
     return (x':rest)
 
-step :: Int -> IOUArray Int Int -> Int -> IO Int
-step m buffer n = do
+step :: IOUArray Int Int -> Int -> IO Int
+step buffer n = do
     [a, b, c, n'] <- follow 4 buffer n
+    (_, m) <- getBounds buffer
     let insertPoint = findNext m (n-1) [a, b, c]
     after <- readArray buffer insertPoint
     writeArray buffer insertPoint a
@@ -40,8 +39,10 @@ step m buffer n = do
     writeArray buffer n n'
     return n'
 
-buildPermutation :: Int -> [Int] -> IO (IOUArray Int Int)
-buildPermutation m elements = MArray.newListArray (1,m) $ map snd $ sort $ zip elements (tail elements ++ [head elements])
+buildPermutation :: [Int] -> IO (IOUArray Int Int)
+buildPermutation elements = MArray.newListArray (1,upper) $ map snd $ sort $ zip elements (tail elements ++ [head elements])
+    where
+        upper = length elements
 
 sequenceFrom :: Int -> Array Int Int -> [Int]
 sequenceFrom start array = sequenceFrom' start
@@ -61,12 +62,12 @@ repeatM n f x = do
 main :: IO ()
 main = do
     sequence <- map digitToInt . head <$> getArgs
-    buffer <- buildPermutation 9 sequence
-    repeatM 100 (step 9 buffer) (head sequence) 
+    buffer <- buildPermutation sequence
+    repeatM 100 (step buffer) (head sequence) 
     sequence' <- sequenceFrom 1 <$> freeze buffer
     putStrLn $ concatMap show $ tail sequence'
-    buffer <- buildPermutation 1000000 $ sequence ++ [10..1000000]
-    repeatM 10000000 (step 1000000 buffer) (head sequence)
+    buffer <- buildPermutation $ sequence ++ [10..1000000]
+    repeatM 10000000 (step buffer) (head sequence)
     sequence' <- sequenceFrom 1 <$> freeze buffer
     print $ (sequence' !! 1) * (sequence' !! 2)
 
